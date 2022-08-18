9e90ea49456d4fde52e6f6331de1f84ef33a32ad
AVJetBrains <96913572+AVJetBrains@users.noreply.github.com>
2022-08-19 02:22:53 +0530

    Initial

--- src/main/java/com/example/hrmanagementfinal/security/UserSecurityService.java
+++ src/main/java/com/example/hrmanagementfinal/security/UserSecurityService.java
@@ -6,12 +6,14 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
 public class UserSecurityService implements UserDetailsService {
 
-    private com.example.hrmanagementfinal.security.UserSecurityService userService;
+    private com.example.hrmanagementfinal.services.UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
